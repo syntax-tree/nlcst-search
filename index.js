4481c07d13908f2c934a40f8af6e8293f31cbe73
Titus Wormer <tituswormer@gmail.com>
2023-07-17 15:46:54 +0200

    Remove deprecated `SearchOptions` type

--- index.js
+++ index.js
@@ -3,10 +3,6 @@
  * @typedef {import('./lib/index.js').Options} Options
  * @typedef {import('./lib/index.js').PhrasesList} PhrasesList
  * @typedef {import('./lib/index.js').PhrasesMap} PhrasesMap
- *
- * @typedef {import('./lib/index.js').Options} SearchOptions
- *   Deprecated form of `Options`.
  */
-// To do: next major: remove `SearchOptions`.
 
 export {search} from './lib/index.js'
